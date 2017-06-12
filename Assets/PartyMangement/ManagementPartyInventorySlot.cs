eab4dfe04c2f60846db2f55d6debd7a0d987ff99
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2017-06-12 14:39:07 -0400

    Fixes management hiding images that shouldn't be shown

--- Assets/PartyMangement/ManagementPartyInventorySlot.cs
+++ Assets/PartyMangement/ManagementPartyInventorySlot.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class ManagementPartyInventorySlot : MonoBehaviour {
 	public void Setup(SessionManager session, PartyManagement management, int myIndex){
 		if (session.state.inventory.Count <= myIndex) {
-			GetComponent<SpriteRenderer>().color = Color.magenta;
+			GetComponent<SpriteRenderer>().color = Color.clear;
 			return;
 		}
 		Item myItem = session.state.inventory[myIndex];
+		GetComponent<SpriteRenderer>().color = Color.white;
 		GetComponent<SpriteRenderer>().sprite = myItem.GetDef(session).image;
 	}
 	public void SetupAsPartyInventory(SessionManager session, PartyManagement management, int partyIndex, int inventoryIndex){
 		PartyMember pm = session.state.party[partyIndex];
 		if (pm.inventory.Count <= inventoryIndex) {
-			GetComponent<SpriteRenderer>().color = Color.magenta;
+			GetComponent<SpriteRenderer>().color = Color.clear;
 			return;
 		}
 		Item myItem = pm.inventory[inventoryIndex];
+		GetComponent<SpriteRenderer>().color = Color.white;
 		GetComponent<SpriteRenderer>().sprite = myItem.GetDef(session).image;
 	}
 }
